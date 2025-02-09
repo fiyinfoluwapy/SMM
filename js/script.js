24c7746e9fab954485d978ad04d7d564d24fb2db
Temidire Damian <vladimirwayne9@gmail.com>
2025-02-09 17:23:23 +0000

    skill set section

--- js/script.js
+++ js/script.js
@@ -117,4 +117,5 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         });
     }
+
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primaryOrange: "#ff6c1f",
-        totemPole: "#AD2509",
-        easternBlue: "#1990A8",
-        daisyBlush: "#4519A8",
-        tomarillo: "#A8191B",
-        primaryWhite: "#f1f1f1",
-        primaryBlack: "#000000",
-        sharpWhite: "#ffffff",
+        primaryOrange: " #ff6c1f",
+        totemPole: " #AD2509",
+        easternBlue: " #1990A8",
+        daisyBlush: " #4519A8",
+        tomarillo: " #A8191B",
+        primaryWhite: " #f1f1f1",
+        primaryBlack: " #000000",
+        sharpWhite: " #ffffff",
       },
       fontFamily: {
         bitter: ['Bitter', 'serif'],
